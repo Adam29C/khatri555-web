2f569dee775ab4d2068699bdd5b5d1c496b3a8c1
Adam29C <adamcatholic29@gmail.com>
2024-10-07 16:15:44 +0530

    update build

--- src/components/Config/config.js
+++ src/components/Config/config.js
@@ -2,6 +2,6 @@
 // export const base_url = "http://51.20.125.209:5000/"
 // export const base_url = "http://16.171.241.112:5000/"
 // export const base_url = "http://13.51.64.99:5000/"
-export const base_url = "http://16.170.215.79:5000/"
+// export const base_url = "http://16.170.215.79:5000/"
 
-// export const base_url = "https://api.khatri555.com/"
+export const base_url = "https://api.khatri555.com/"
