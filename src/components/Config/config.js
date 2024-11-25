45f8efb8e937227969256c04c4fba0f8392ae59d
Adam29C <adamcatholic29@gmail.com>
2024-11-25 12:43:32 +0530

    update local url buld

--- src/components/Config/config.js
+++ src/components/Config/config.js
@@ -3,4 +3,6 @@
 // export const base_url = "http://16.171.241.112:5000/"
 // export const base_url = "http://13.51.64.99:5000/"
 // export const base_url = "http://16.170.215.79:5000/"
-export const base_url = "https://api.khatri555.com/"
+// export const base_url = "https://api.khatri555.com/"
+export const base_url = "http://16.171.226.214:5000/"
+
